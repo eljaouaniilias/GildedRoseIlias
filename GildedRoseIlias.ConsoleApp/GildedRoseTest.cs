b48ef5e393cc2a4b40e25452f18f44e405c72ad1
Ilias El Jaouani <ilias.el.jaouani@apb.be>
2020-11-14 21:39:58 +0100

    Add project files.

---
+++ GildedRoseIlias.ConsoleApp/GildedRoseTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using GildedRoseIlias.Library;
+
+namespace GildedRoseIlias.ConsoleApp
+{
+    [TestFixture]
+    public class GildedRoseTest
+    {
+        [Test]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("fixme", Items[0].Name);
+        }
+    }
+}
